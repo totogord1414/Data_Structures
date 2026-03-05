e0880e1c35b52f36f4262482d3a0acf1897031a0
totogord1414 <zhanzitong021@gmail.com>
2026-03-05 17:35:26 +0800

    2026.3.5 get 1001 completed

--- 1001.cpp
+++ 1001.cpp
@@ -1,3 +1,5 @@
+//栈版本
+/*
 #include<iostream>
 #include<vector>
 
@@ -6,6 +8,65 @@ using namespace std;
 int main()
 {
     int num = 0;
-    cin>>num;
+    cin >> num;
+    vector<int> n;
+    vector<int> n_count;
+    for(int i = 0;i < num;i++)
+    {
+        int n_temp;
+        cin >> n_temp;
+        n_count.push_back(n_temp);
+        for(int j = 0;j < n_count[i];j++)
+        {
+            int temp;
+            cin >> temp;
+            n.push_back(temp);
+        }
+    }
+    for(int i = num;i;i--)
+    {
+        for(int j = n_count[i - 1];j;j--)
+        {
+            cout << n.back() << " ";
+            n.pop_back();
+        }
+        cout << "\n";
+    }
+    return 0;
 
+}
+    */
+
+//二维vector
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    vector<vector<int>> data(num);//通过添加num,预留空间，防止因为push_back后内存不够导致搬家，损失性能
+    for(int i = 0;i < num;i++)
+    {
+        int c;
+        cin >> c;
+        data[i].resize(c);
+        for(int j = 0;j < c;j++)
+        {
+            cin >> data[i][j];
+        }
+    }
+    for(int i = num-1 ;i >= 0;i--)
+    {
+        for(int j = data[i].size() - 1;j >= 1;j--)
+        {
+            cout << data[i][j] << " ";
+        }
+        cout << data[i][0] << "\n";
+    }
+    return 0;
 }
---
+++ 1002.cpp
@@ -0,0 +1,26 @@
+//手动指针遍历
+
+#include <iostream>
+
+#define MAXTOKEN 105
+
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    cin >> count;
+    for(int i = 0;i < count;i++)
+    {
+        int w_count = 0;
+        char s[MAXTOKEN];
+        char target[10];
+        cin >> s;
+        cin >> target;
+        while(s++ != '\0')
+        {
+            
+        }
+        
+    }
+}
