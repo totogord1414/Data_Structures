acd6d174983a5d603f9bd6439e314c68a361583c
totogord1414 <zhanzitong021@gmai.com>
2026-07-20 21:42:35 +0800

    core algorithm completed

--- summer_project/multi/菜鸟驿站配送项目/src/core/TaskSolver.cpp
+++ summer_project/multi/菜鸟驿站配送项目/src/core/TaskSolver.cpp
@@ -1,5 +1,9 @@
 #include "../../include/core/TaskSolver.h"
+#include "models/Package.h"
 #include <iostream>
+#include <algorithm>
+
+const int INF = 1e9;
 
 namespace core {
 
@@ -26,8 +30,60 @@ namespace core {
     }
 
     void TaskSolver::solveT2() {
-        // TODO: Implement T2 logic
-        std::cout << "T2 not implemented yet." << std::endl;
+        
+        std::vector<models::Package> pending = packages;
+        
+        std::sort(pending.begin(), pending.end(), [](const models::Package& a, const models::Package& b) {
+            return a.arrive_time < b.arrive_time;
+        });
+
+        double totalDissatisfaction = 0.0;
+        double currentTime = 0.0;
+
+        while (!pending.empty()) {
+            std::vector<models::Package> currCarLoad;
+            double currLoadWeight = 0.0;
+            double max_time = 0.0;
+
+            while (!pending.empty() && currLoadWeight + pending.front().weight <= car.capacity) {
+                currLoadWeight += pending.front().weight;
+                max_time = pending.front().arrive_time;
+                currCarLoad.push_back(pending.front());
+                pending.erase(pending.begin());
+
+            }
+
+            currentTime = std::max(max_time, currentTime);
+
+            int currPos = 0;
+
+            while (!currCarLoad.empty()) {
+                int bestIndex = -1;
+                double shortestDist = INF;
+                for (int i = 0; i < currCarLoad.size(); i++) {
+                    double dist = router.getShortestPath(currPos, currCarLoad[i].dest).distance;
+
+                    if (dist < shortestDist) {
+                        shortestDist = dist;
+                        bestIndex = i;
+                    }
+                }
+
+                models::Package targetPackage = currCarLoad[bestIndex];
+
+                currentTime += shortestDist / car.speed;
+
+                totalDissatisfaction += (currentTime - targetPackage.arrive_time);
+                currPos = targetPackage.dest;
+                currCarLoad.erase(currCarLoad.begin() + bestIndex);
+            }
+
+            double dist = router.getShortestPath(currPos, 0).distance;
+
+            currentTime += dist / car.speed;
+        }
+
+        std::cout << "The total dissatisfactio through T2's algorithm is: " << totalDissatisfaction << std::endl;
     }
 
     void TaskSolver::solveT3() {
